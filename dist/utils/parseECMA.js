841aa28e1ce96e925fcc87507bd4ca3e6a28f200
Luc Belliveau <luc.belliveau@nrc-cnrc.gc.ca>
2018-07-20 12:37:29 -0300

    replaced esprima-fb with estree to support JSX fragments

--- dist/utils/parseECMA.js
+++ dist/utils/parseECMA.js
@@ -3,19 +3,36 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
+
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
+
 exports.default = parseECMA;
-
-var _esprimaFb = require('esprima-fb');
-
-var _esprimaFb2 = _interopRequireDefault(_esprimaFb);
 
 var _ramda = require('ramda');
 
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+var espree = require('espree');
+
+var fixTraversal = function fixTraversal(obj, parents) {
+  if (obj && obj.type === 'CallExpression') {
+    delete obj.start;
+    for (var x in parents) {
+      delete parents[x].start;
+    }
+  }
+  for (var i in obj) {
+    if (obj[i] && _typeof(obj[i]) === 'object') {
+      fixTraversal(obj[i], parents.concat([obj]));
+    }
+  }
+  return obj;
+};
 
 function parseECMA(source) {
-  return (0, _ramda.prop)('body')(_esprimaFb2.default.parse(source, {
-    tolerant: true,
-    loc: true
-  }));
+  return fixTraversal((0, _ramda.prop)('body')(espree.parse(source, {
+    sourceType: 'module',
+    loc: true,
+    ecmaFeatures: {
+      jsx: true
+    }
+  })), []);
 }
--- src/utils/parseECMA.js
+++ src/utils/parseECMA.js
@@ -1,11 +1,29 @@
-import esprima from 'esprima-fb';
 import {prop} from 'ramda';
+const espree = require('espree');
+
+const fixTraversal = (obj, parents) => {
+  if (obj && obj.type === 'CallExpression') {
+    delete obj.start;
+    for (const x in parents) {
+      delete parents[x].start;
+    }
+  }
+  for (const i in obj) {
+    if (obj[i] && typeof obj[i] === 'object') {
+      fixTraversal(obj[i], parents.concat([obj]));
+    }
+  }
+  return obj;
+}
 
 export default function parseECMA(source){
-  return prop('body')(
-    esprima.parse(source, {
-      tolerant: true,
-      loc: true
+  return fixTraversal(prop('body')(
+      espree.parse(source, {
+      sourceType: 'module',
+      loc: true,
+      ecmaFeatures: {
+        jsx: true,
+      }
     })
-  );
+  ), []);
 }
